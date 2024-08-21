a970b247b53ef26eb4420fde6fd69bdaee30e965
Edstorm17 <edstorm17@gmail.com>
2024-08-21 17:01:47 -0400

    Added some stuff

--- scripts/login_content.js
+++ scripts/login_content.js
@@ -10,8 +10,10 @@ function initLogin(theme, hideLoginLogo, cleanLogin, compactCommuniques) {
 
     // Login logo
     const logo = document.querySelector(".login-logoLogin");
+    if (logo) {
+        logo.src = chrome.runtime.getURL('images/login_logo_' + theme + '.png');
+    }
 
-    logo.src = chrome.runtime.getURL('images/login_logo_' + theme + '.png');
     document.querySelector(".login-logoEntete").src = chrome.runtime.getURL('images/header_logo_' + theme + '.png');
 
     if (hideLoginLogo) {
@@ -29,34 +31,37 @@ function initLogin(theme, hideLoginLogo, cleanLogin, compactCommuniques) {
 
         // First contents
         const firstSep = document.querySelector(".pagecoba > .rangee:has(> div > .login-sep)");
-        const firstContents = [];
-        let current;
-        for (current = firstSep.nextElementSibling; current && current.firstElementChild.nodeName.toLowerCase() !== "div"; current = current.nextElementSibling) {
-            firstContents.push(current);
+        if (firstSep) {
+            const firstContents = [];
+            let current;
+            for (current = firstSep.nextElementSibling; current && current.firstElementChild.nodeName.toLowerCase() !== "div"; current = current.nextElementSibling) {
+                firstContents.push(current);
+            }
+
+            // Second contents
+            const secondContents = [];
+            for (current = current.nextElementSibling; current && current.firstElementChild.nodeName.toLowerCase() !== "div"; current = current.nextElementSibling) {
+                secondContents.push(current);
+            }
+
+            // Assemble page
+            const lastSep = current;
+            const mainContainer = document.createElement("div");
+            const leftContainer = document.createElement("div");
+            const rightContainer = document.createElement("div");
+            mainContainer.classList.add("communiquesContainer");
+            for (const element in firstContents) {
+                leftContainer.appendChild(firstContents[element]);
+            }
+            for (const element in secondContents) {
+                rightContainer.appendChild(secondContents[element]);
+            }
+            mainContainer.appendChild(leftContainer);
+            mainContainer.appendChild(rightContainer);
+            firstSep.insertAdjacentElement("afterend", mainContainer);
+            lastSep.style.display = "none";
         }
 
-        // Second contents
-        const secondContents = [];
-        for (current = current.nextElementSibling; current && current.firstElementChild.nodeName.toLowerCase() !== "div"; current = current.nextElementSibling) {
-            secondContents.push(current);
-        }
-
-        // Assemble page
-        const lastSep = current;
-        const mainContainer = document.createElement("div");
-        const leftContainer = document.createElement("div");
-        const rightContainer = document.createElement("div");
-        mainContainer.classList.add("communiquesContainer");
-        for (const element in firstContents) {
-            leftContainer.appendChild(firstContents[element]);
-        }
-        for (const element in secondContents) {
-            rightContainer.appendChild(secondContents[element]);
-        }
-        mainContainer.appendChild(leftContainer);
-        mainContainer.appendChild(rightContainer);
-        firstSep.insertAdjacentElement("afterend", mainContainer);
-        lastSep.style.display = "none";
     }
 
 }
