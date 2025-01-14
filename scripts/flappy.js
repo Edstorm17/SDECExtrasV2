67e17c7643bdb211153ca4a86274db216cff5a82
BuildTools <unconfigured@null.spigotmc.org>
2025-01-14 18:23:13 -0500

    Fixed flappy, again

--- scripts/flappy.js
+++ scripts/flappy.js
@@ -1,134 +1,125 @@
 
-const move_speed = 3;
+const end_message = 'Appuyez sur entrée pour réessayer';
 
-const gravity = 0.20;
+const speed = 3;
+const gravity = 0.2;
+const jump_velocity = -7.6;
+const pipe_gap = 35;
 
-let bird = document.querySelector('.bird');
+let bird = document.querySelector(".bird");
+let bird_box = bird.getBoundingClientRect();
+let background_box = document.querySelector(".background").getBoundingClientRect();
 
-let bird_props = bird.getBoundingClientRect();
-let background = document.querySelector('.background').getBoundingClientRect();
-
-let score_val = document.querySelector('.score_val');
-let message = document.querySelector('.message');
+let score_value = document.querySelector(".score_value");
+let title = document.querySelector(".title");
 let score_title = document.querySelector('.score_title');
 
-let game_state = 'Start';
+let game_state = 'start';
+let bird_dy = 0;
+let pipe_separation = 0;
 
-document.addEventListener('keydown', (e) => {
-    if (e.key === 'Enter') startHandler(e);
+document.addEventListener('keydown', e => {
+    if (e.key === 'Enter') start();
+    else if (e.key === 'ArrowUp' || e.key === ' ') click();
 });
-document.addEventListener('pointerdown', startHandler);
+document.addEventListener('pointerdown', () => {
+    click();
+    start();
+});
 
-function startHandler() {
-    if (game_state !== 'Play') {
-        document.querySelectorAll('.pipe_sprite').forEach((e) => e.remove());
-        bird.style.top = '40vh';
-        game_state = 'Play';
-        message.innerHTML = '';
-        score_title.innerHTML = 'Score : ';
-        score_val.innerHTML = '0';
-        bird_props = bird.getBoundingClientRect();
-        play();
-    }
+function start() {
+    if (game_state === 'play') return;
+
+    document.querySelectorAll('.pipe').forEach((e) => e.remove());
+    bird.style.top = '40vh';
+    game_state = 'play';
+    title.innerHTML = '';
+    score_title.innerHTML = 'Score : ';
+    score_value.innerHTML = '0';
+    bird_box = bird.getBoundingClientRect();
+    bird_dy = 0;
+    requestAnimationFrame(loop);
 }
 
-function play() {
-    console.log(game_state)
-    function move() {
+function movePipes() {
+    let pipes = document.querySelectorAll('.pipe');
+    pipes.forEach(pipe => {
+        let pipe_box = pipe.getBoundingClientRect();
+        bird_box = bird.getBoundingClientRect();
 
-        if (game_state !== 'Play') return;
-
-        let pipe_sprite = document.querySelectorAll('.pipe_sprite');
-        pipe_sprite.forEach((element) => {
-
-            let pipe_sprite_props = element.getBoundingClientRect();
-            bird_props = bird.getBoundingClientRect();
-
-            if (pipe_sprite_props.right <= 0) {
-                element.remove();
+        if (pipe_box.right <= 0) pipe.remove();
+        else {
+            if (
+                bird_box.left < pipe_box.left + pipe_box.width &&
+                bird_box.left + bird_box.width > pipe_box.left &&
+                bird_box.top < pipe_box.top + pipe_box.height &&
+                bird_box.top + bird_box.height > pipe_box.top
+            ) {
+                game_state = 'end';
+                title.innerHTML = 'Appuyez sur entrée pour réessayer';
+                title.style.left = '28vw';
             } else {
                 if (
-                    bird_props.left < pipe_sprite_props.left +
-                    pipe_sprite_props.width &&
-                    bird_props.left +
-                    bird_props.width > pipe_sprite_props.left &&
-                    bird_props.top < pipe_sprite_props.top +
-                    pipe_sprite_props.height &&
-                    bird_props.top +
-                    bird_props.height > pipe_sprite_props.top
+                    pipe_box.right < bird_box.left &&
+                    pipe_box.right + speed >= bird_box.left &&
+                    pipe.score
                 ) {
-                    game_state = 'End';
-                    message.innerHTML = 'Appuyez sur entrée pour recommencer';
-                    message.style.left = '28vw';
-                } else {
-                    if (
-                        pipe_sprite_props.right < bird_props.left &&
-                        pipe_sprite_props.right +
-                        move_speed >= bird_props.left &&
-                        element.increase_score === '1'
-                    ) {
-                        score_val.innerHTML = +score_val.innerHTML + 1;
-                    }
-                    element.style.left = pipe_sprite_props.left - move_speed + 'px';
+                    score_value.innerHTML = +score_value.innerHTML + 1;
                 }
+                pipe.style.left = pipe_box.left - speed + 'px';
             }
-        });
-
-        requestAnimationFrame(move);
-    }
-    requestAnimationFrame(move);
-
-    let bird_dy = 0;
-    function apply_gravity() {
-        if (game_state !== 'Play') return;
-        bird_dy = bird_dy + gravity;
-
-        document.addEventListener('keydown', (e) => {
-            if (e.key === 'ArrowUp' || e.key === ' ') pressHandler(e);
-        });
-        document.addEventListener('pointerdown', pressHandler);
-        function pressHandler() {
-            bird_dy = -7.6;
         }
+    });
+}
 
-        if (bird_props.top <= 0 || bird_props.bottom >= background.bottom) {
-            game_state = 'End';
-            message.innerHTML = 'Appuyez sur entrée pour recommencer';
-            message.style.left = '28vw';
-            return;
-        }
-        bird.style.top = bird_props.top + bird_dy + 'px';
-        bird_props = bird.getBoundingClientRect();
-        requestAnimationFrame(apply_gravity);
+function applyGravity() {
+    bird_dy += gravity;
+
+    if (bird_box.top <= 0 || bird_box.bottom >= background_box.bottom) {
+        game_state = 'end';
+        title.innerHTML = end_message;
+        title.style.left = '28vw';
+        return;
     }
-    requestAnimationFrame(apply_gravity);
+    bird.style.top = bird_box.top + bird_dy + 'px';
+    console.log(bird.style.top);
+    bird_box = bird.getBoundingClientRect();
+}
 
-    let pipe_separation = 0;
+function createPipe() {
+    if (pipe_separation > 200) {
+        pipe_separation = 0;
+        let pipe_pos = Math.floor(Math.random() * 43) + 8;
 
-    let pipe_gap = 35;
-    function create_pipe() {
-        if (game_state !== 'Play') return;
+        let pipe_inv = document.createElement('div');
+        pipe_inv.className = 'pipe';
+        pipe_inv.style.top = pipe_pos - 70 + 'vh';
+        pipe_inv.style.left = '100vw';
+        document.body.appendChild(pipe_inv);
 
-        if (pipe_separation > 200) {
-            pipe_separation = 0;
-
-            let pipe_posi = Math.floor(Math.random() * 43) + 8;
-            let pipe_sprite_inv = document.createElement('div');
-            pipe_sprite_inv.className = 'pipe_sprite';
-            pipe_sprite_inv.style.top = pipe_posi - 70 + 'vh';
-            pipe_sprite_inv.style.left = '100vw';
-
-            document.body.appendChild(pipe_sprite_inv);
-            let pipe_sprite = document.createElement('div');
-            pipe_sprite.className = 'pipe_sprite';
-            pipe_sprite.style.top = pipe_posi + pipe_gap + 'vh';
-            pipe_sprite.style.left = '100vw';
-            pipe_sprite.increase_score = '1';
-
-            document.body.appendChild(pipe_sprite);
-        }
-        pipe_separation++;
-        requestAnimationFrame(create_pipe);
+        let pipe = document.createElement('div');
+        pipe.className = 'pipe';
+        pipe.style.top = pipe_pos + pipe_gap + 'vh';
+        pipe.style.left = '100vw';
+        pipe.score = true;
+        document.body.appendChild(pipe);
     }
-    requestAnimationFrame(create_pipe);
+    pipe_separation++;
+}
+
+function loop() {
+    if (game_state !== 'play') return;
+
+    movePipes();
+    applyGravity();
+    createPipe();
+
+    requestAnimationFrame(loop);
+}
+
+function click(e) {
+    if (game_state !== 'play') return;
+    e.preventDefault();
+
+    bird_dy = jump_velocity;
 }
